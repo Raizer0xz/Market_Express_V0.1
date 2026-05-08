a610c5d7ab3e20bbc1b050b66bf412f7b558cf01
omar_ <oma.pina@duocuc.cl>
2026-05-08 13:28:34 -0400

    Mi Primer Commit

---
+++ MS-eureka-server/src/main/java/com/example/MS_eureka_server/MsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MS_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaServerApplication.class, args);
+	}
+
+}
---
+++ MS-sucursales/src/main/java/com/example/MS_sucursales/MsSucursalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.MS_sucursales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSucursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSucursalesApplication.class, args);
+	}
+
+}
