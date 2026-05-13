d2a9695195b81522ccbe3b390365ce3fe2068078
URDEON <edua.aguirre@duocuc.cl>
2026-05-13 18:35:19 -0400

    creacion de ms carrito

--- MS-carrito/src/main/java/com/example/MS_carrito/MsCarritoApplication.java
+++ MS-carrito/src/main/java/com/example/MS_carrito/MsCarritoApplication.java
@@ -2,12 +2,12 @@ package com.example.MS_carrito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsCarritoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsCarritoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsCarritoApplication.class, args);
+    }
 }
