a68ee15562716398a2582d65adc2416b9d008004
omar_ <oma.pina@duocuc.cl>
2026-05-13 18:32:12 -0400

    falta probar el ms no se puede ejecutar por falta de gateway

--- MS-seguridad/src/main/java/com/example/MS_gateway_security/MsGatewaySecurityApplication.java
+++ MS-seguridad/src/main/java/com/example/MS_gateway_security/MsGatewaySecurityApplication.java
@@ -2,8 +2,12 @@ package com.example.MS_gateway_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MsGatewaySecurityApplication {
 
 	public static void main(String[] args) {
